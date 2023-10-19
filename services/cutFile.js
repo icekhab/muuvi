d012e1176cb61749312528c515fd7cb9ac0b1cb2
eugeneopryshko <e.opryshko@amai.io>
2023-10-19 15:16:23 +0400

    Fix

--- services/cutFile.js
+++ services/cutFile.js
@@ -4,19 +4,22 @@ const Stream = require('stream');
 const spawn = require('child_process').spawn;
 
 module.exports = async (payload, callback) => {
-    const readableStream = new Stream.Readable()
-    readableStream._read = () => {}
+    const readableStream = new Stream.Readable({
+        read() {}
+    });
+
     try {
         const { top = 0, bottom = 0, left = 0, right = 0 } = payload.cutPoints || {};
         const width = 1 - (left/100) - (right/100);
         const height = 1 - (top/100) - (bottom/100);
+
         let info = await ytdl.getInfo(payload.link);
         let format = ytdl.chooseFormat(info.formats, { quality: 'highest', filter: 'audioandvideo' });
 
         const args = [
             '-ss', payload.startTime,
-            '-i', format.url,
             '-t', payload.duration,
+            '-i', format.url,
             '-filter:v', `crop=iw*${width}:ih*${height}:iw*${left / 100}:ih*${top / 100}`,
             '-movflags', 'frag_keyframe+empty_moov',
             '-f', 'mp4', 'pipe:1'
@@ -25,17 +28,13 @@ module.exports = async (payload, callback) => {
         const proc = spawn(ffmpegPath, args);
 
         proc.on('error', (err) => {
-            console.log('Cut file error', err)
+            console.log('Cut file error', err);
             callback(err);
         });
         proc.stderr.setEncoding('utf8');
         let output = '';
-        proc.stderr
-            .on('data', c => { output += c; });
-        proc.stdout
-            .on('data', c => {
-                readableStream.push(c);
-            });
+        proc.stderr.on('data', c => { output += c; });
+        proc.stdout.on('data', c => { readableStream.push(c); });
 
         proc.on('exit', (code) => {
             if (code) {
@@ -47,7 +46,7 @@ module.exports = async (payload, callback) => {
         });
 
         proc.on('close', function() {
-            console.log('kill')
+            console.log('kill');
             proc.kill();
         });
     } catch(err) {
